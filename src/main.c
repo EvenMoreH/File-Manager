1f30a2d4b433b936b9c548d227dc19f411be25ff
EvenMoreH <hubert.lewan@gmail.com>
2024-07-17 17:18:17 +0200

    It works great however without integration with reading metadata it is no use for me now, I will pick it up later, but it was fun!

--- src/main.c
+++ src/main.c
@@ -2,38 +2,41 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>      // allows for tolower() funtion
+
+DIR *directoryPointer;
+struct dirent *entry;
+char input[256];
+char leave;
 
 int main() {
 
-    DIR *directoryPointer;
-    struct dirent *entry;
-    char input[256];
-
-    printf("Input directory using format:\nC:/Example/Example2/\n> ");
-    // scanf("%s", &path);
-    // fgets has a \n at the end by default so it does not work
-    fgets(input, 256, stdin);
-    int len = (strlen(input)) - 1;
-    char path[len];
-    memcpy(path, input, len);
-    // printf("%s\n", path);
-
-    directoryPointer = opendir(path);
-    if (directoryPointer == NULL)
+    do
     {
-        perror("opendir");
-    }
+        printf("Input directory using format:\nC:/Example/Example2\n> ");
+        scanf("%255[^\n]", input);
 
-    printf("Contents of the \"%s\" directory:\n", path);
-    while ((entry = readdir(directoryPointer)) != NULL)
-    {
-        printf("%s\n", entry->d_name);
-    }
+        directoryPointer = opendir(input);
+        if (directoryPointer == NULL)
+        {
+            perror("opendir");
+        }
 
-    if (closedir(directoryPointer) != 0)
-    {
-        perror("closedir");
-    }
+        printf("Contents of the \"%s\" directory:\n", input);
+        while ((entry = readdir(directoryPointer)) != NULL)
+        {
+            printf("%s\n", entry->d_name);
+        }
+
+        if (closedir(directoryPointer) != 0)
+        {
+            perror("closedir");
+        }
+        printf("\n------------------------------------------");
+        printf("\n\nPress Y to exit.\n> ");
+        scanf(" %1c", &leave);
+        leave = tolower(leave);
+    } while (leave != 'y');
 
     return 0;
 }
